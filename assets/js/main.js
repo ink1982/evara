35998fa01e45d251312f78f929e09829857ade0e
ink1982 <ink1982@outlook.com>
2025-03-21 17:55:04 +0200

    products completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,20 @@ var swiperCategories = new Swiper('.categories__container', {
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[content]');
+
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.target);
+		tabContents.forEach(tabContent => {
+			tabContent.classList.remove('active-tab');
+		});
+		target.classList.add('active-tab');
+
+		tabs.forEach(tab => {
+			tab.classList.remove('active-tab');
+		});
+		tab.classList.add('active-tab');
+	});
+});
